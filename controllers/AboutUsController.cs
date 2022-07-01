ba2f516acc9cef0785219589e637074c846d4e09
aljones0206 <99285849+aljones0206@users.noreply.github.com>
2022-07-01 13:37:49 -0400

    Add files via upload

---
+++ controllers/AboutUsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web2.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            Models.User u = new Models.User();
+
+            u.FirstName = "Amanda";
+            u.LastName = "Wittwer";
+            u.Email = "alwittwer@cincinnatistate.edu";
+
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection col)
+        {
+            try
+            {
+                Models.User u = new Models.User();
+
+                if (col["btnSubmit"] == "more")
+                { //more button pressed
+                    return RedirectToAction("More");
+                }
+                else if (col["btnClose"] == "close")
+                { //Close button pressed
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View(u);
+                }
+            }
+            catch (Exception)
+            {
+                Models.User u = new Models.User();
+                return View(u);
+            }
+
+        }
+
+        // GET: More
+        public ActionResult More()
+        {
+
+            Models.User u = new Models.User();
+
+            u.FirstName = "Amanda";
+            u.LastName = "Wittwer";
+            u.Email = "alwittwer@cincinnatistate.edu";
+
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult More(FormCollection col)
+        {
+            return RedirectToAction("Index");
+
+        }
+    }
+}
