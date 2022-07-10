dbf69e4e62aa9b23af27dd5e3f41f398d771865a
Nick Kohlman <100505223+Nick-Kohlman@users.noreply.github.com>
2022-07-10 17:05:23 -0400

    Update User.cs

--- models/User.cs
+++ models/User.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
 
-namespace web2.Models
+namespace QuickClean.Models
 {
 
 	public class User
