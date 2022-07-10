cdf5cc18701c3ace039f43238b9c955c52049a41
Nick Kohlman <100505223+Nick-Kohlman@users.noreply.github.com>
2022-07-10 17:02:01 -0400

    Update ProfileController.cs

--- controllers/ProfileController.cs
+++ controllers/ProfileController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
 
-namespace web2.Controllers
+namespace QuickClean.Controllers
 {
 	public class ProfileController : Controller
 	{
