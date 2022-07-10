ac798ecdca550a8ff1b11b5cf7bc862c7704184b
Nick Kohlman <100505223+Nick-Kohlman@users.noreply.github.com>
2022-07-10 17:03:30 -0400

    Update JobOrder.cs

--- models/JobOrder.cs
+++ models/JobOrder.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace web2.Models
+namespace QuickClean.Models
 {
     public class JobOrder
     {
